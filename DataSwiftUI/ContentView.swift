0cefb6e17bea7a5fbd8441c571e6bb1b34c7f656
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-22 19:25:48 +0300

    Bind Value for textfields, state wrapper used.

--- DataSwiftUI/ContentView.swift
+++ DataSwiftUI/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isim = "Omer Faruk Kılıçaslan"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            Text(isim)
+                .font(.largeTitle)
+                .padding()
+            
+            Button {
+                self.isim = "OmerFUK"
+            } label: {
+                Text("İsmi Değiştir")
+                    .frame(width: 200, height: 50)
+                    .background(.red)
+                    .foregroundColor(.white)
+            }
+            
+            
+            TextField("Test", text: self.$isim)
+                .padding(.horizontal, 20)
+                .frame(height: 50)
+                .cornerRadius(15)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.red)
+
+        }
     }
 }
 
