cb0b7449e3f69a1bfc58f5f18ee132040a3ef247
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-22 19:12:06 +0300

    Initial Commit

---
+++ DataSwiftUI/DataSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataSwiftUIApp.swift
+//  DataSwiftUI
+//
+//  Created by Ömer Faruk Kılıçaslan on 22.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct DataSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
